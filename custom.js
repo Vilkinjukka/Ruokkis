f095c15ea194b5d1e45bc1d5934c9e7982f688ee
Vilkinjukka <45997665+Vilkinjukka@users.noreply.github.com>
2020-10-20 23:59:58 +0300

    Add files via upload

--- custom.js
+++ custom.js
@@ -1,62 +1,46 @@
+$('body').css('padding-top', $('.navbar').outerHeight() + 'px')
 
+if ($('.smart-scroll').length > 0) { // check if element exists
+    var last_scroll_top = 0;
+    $(window).on('scroll', function() {
+        scroll_top = $(this).scrollTop();
+        if(scroll_top < last_scroll_top) {
+            $('.smart-scroll').removeClass('scrolled-down').addClass('scrolled-up');
+        }
+        else {
+            $('.smart-scroll').removeClass('scrolled-up').addClass('scrolled-down');
+        }
+        last_scroll_top = scroll_top;
+    });
+}
 
-var row = 0;
-var col = 0;
 var dummyDiv = null;
 $(function()
 {
+  console.log(DataBase.length)
   //Load card to main html
   dummyDiv = $("<div id='dummyDiv'/>").css("display","none");
   dummyDiv.load('card.html', function() {
-    ChangeData("Liha-makaronilaatikko", "Kuka nyt ei pitäisi perinteisestä Suomalaisesta makaronilaatikosta!");
-    ChangeData("Lasagne", "Desc");
-    ChangeData("Miilunpolttajan pasta", "Desc");
-    ChangeData("Kinkkukiusaus", "Desc");
-    ChangeData("Itsetehty pitsa", "Desc");
-    ChangeData("Kermainen jauhelihakastike", "Desc");
-    ChangeData("Liha-makaronilaatikko", "Kuka nyt ei pitäisi perinteisestä Suomalaisesta makaronilaatikosta!");
-    ChangeData("Lasagne", "Desc");
-    ChangeData("Miilunpolttajan pasta", "Desc");
-    ChangeData("Kinkkukiusaus", "Desc");
-    ChangeData("Itsetehty pitsa", "Desc");
-    ChangeData("Kermainen jauhelihakastike", "Desc");
-    ChangeData("Liha-makaronilaatikko", "Kuka nyt ei pitäisi perinteisestä Suomalaisesta makaronilaatikosta!");
-    ChangeData("Lasagne", "Desc");
-    ChangeData("Miilunpolttajan pasta", "Desc");
-    ChangeData("Kinkkukiusaus", "Desc");
-    ChangeData("Itsetehty pitsa", "Desc");
-    ChangeData("Kermainen jauhelihakastike", "Desc");
+    var i;
+    for (i = 0; i < DataBase.length; i++) {
+      ChangeData(DataBase[i][0],DataBase[i][1]);
+    }
   });
   dummyDiv.css("display","none");
   $("body").append(dummyDiv);
 
 });
 
-function LataaResepti(id)
-{
-  $("#"+id).load("card.html");
-}
-
 function ChangeData(name,text)
 {
-  if(col == 0)
-  {
-    row = row+1;
-    col = 1
-    console.log("Create new row");
-    $('#grid').append('<div class="row" id="row'+row.toString()+'"></div>');
-  }
-  $('#row'+row.toString()).append('<div class="col-sm" id="col'+col.toString()+'"></div>');
-
   dummyDiv.find(".card-title").html(name);
   dummyDiv.find(".card-text").html(text);
   var edited = dummyDiv.clone()
   edited.css("display","block");
-  $("#row"+row.toString()).find("#col"+col.toString()).append(edited);
+  $("#cardholder").append(edited);
+}
 
-  col = col+1;
-  if(col >= 4)
-  {
-    col = 0;
-  }
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
